461d2932d419a540408e62f24449082a08b5f1aa
tk <kauati.toiti@gmail.com>
2024-07-04 17:54:14 +0100

    home page : start

--- kolab/app/controllers/login_controller.rb
+++ kolab/app/controllers/login_controller.rb
@@ -5,13 +5,14 @@ class LoginController < ApplicationController
   def create
     @user = User.find_by(email: params[:email])
     puts "Found user: #{@user}"
- 
+
     if @user&.authenticate(params[:password])
       session[:user_id] = @user.id
+      puts "User logged in with session ID: #{session[:user_id]}"
       redirect_to home_path, notice: "Successfully logged in!"
     else
       flash.now[:alert] = "Invalid email or password"
-      render :new, status: :unprocessable_entity
+      render :index
     end
   end
 
